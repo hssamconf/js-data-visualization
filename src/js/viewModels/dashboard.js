5f6e8c34cdd09ed3d30b04a37e72c1636dcc2072
Houssam-Eddine RAFIKI <hssamconf@gmail.com>
2018-06-27 13:33:46 +0100

    Finishing scatter chart + select input to switch

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -6,7 +6,7 @@
 /*
  * Your dashboard ViewModel code goes here
  */
-define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojbutton', 'ojs/ojinputtext', 'ojs/ojlabel', 'ojs/ojchart'],
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojbutton', 'ojs/ojinputtext', 'ojs/ojlabel', 'ojs/ojchart', 'ojs/ojselectcombobox'],
     function (oj, ko, $) {
 
         function DashboardViewModel() {
@@ -14,17 +14,28 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojbutton', 'ojs/ojinputtext', '
             // Below are a set of the ViewModel methods invoked by the oj-module component.
             // Please reference the oj-module jsDoc for additional information.
             this.dataAreaChart = ko.observable("Series 1\\tSeries 2\\tSeries 3\\tSeries 4\\n74\\t50\\t34\\t18\\n42\\t58\\t22\\t6\\n70\\t46\\t30\\t14\\n46\\t54\\t32\\t22");
+            this.dataScatterChart = ko.observable("Series 1\\tSeries 2\\tSeries 3\\tSeries 4\\n15,15\\t25,15\\t17,36\\t38,22\\n25,43\\t55,45\\t32,52\\t43,43\\n25,25\\t57,47\\t26,28\\t58,36");
             this.data = ko.observable();
-            self.areaSeriesValue = ko.observableArray([]);
-            self.areaGroupsValue = ko.observableArray([]);
+            this.areaSeriesValue = ko.observableArray();
+            this.areaGroupsValue = ko.observableArray();
+            this.scatterSeriesValue = ko.observableArray();
+            this.scatterGroupsValue = ko.observableArray();
+            this.selectedOption = ko.observable();
 
             self.submitInput = function () {
                 var element1 = document.getElementById("text-input");
                 element1.validate().then(function (result1) {
                     if (result1 === "valid") {
-                        var res = stringToAreaChart(element1.value);
-                        self.areaSeriesValue(res.series);
-                        self.areaGroupsValue(res.groups);
+                        var select1 = document.getElementById("basicSelect");
+                        if (select1.value != null && select1.value === "AREA"){
+                            var res = stringToAreaChart(element1.value);
+                            self.areaSeriesValue(res.series);
+                            self.areaGroupsValue(res.groups);
+                        }else if(select1.value != null && select1.value === "SCATTER"){
+                            var res = stringToScatterChart(element1.value);
+                            self.scatterSeriesValue(res.series);
+                            self.scatterGroupsValue(res.groups);
+                        }
                     }
                 });
             };
@@ -32,19 +43,11 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojbutton', 'ojs/ojinputtext', '
             function stringToAreaChart(data) {
                 var result = {series: [], groups: []};
                 var lines = data.split("\\n");// split the data string into separated lines
-                //console.log("lines");
-                //console.log(lines);
                 var headers = lines[0].split("\\t"); // split the first line that contains series' names
-                //console.log("headers");
-                //console.log(headers);
-
-                for (var i = 0; i < headers.length; i++) {
+                for (var i = 0; i < headers.length; i++)
                     result.series[i] = {name: headers[i].length === 0 ? "Serie " + (i + 1) : headers[i], items: []};
-                }
-
-                for(var i = 1; i<lines.length; i++)
+                for (var i = 1; i < lines.length; i++)
                     result.groups.push("Q" + (i));
-
                 for (var i = 0; i < result.series.length; i++) {
                     for (var j = 1; j < lines.length; j++) {
                         //console.log("L" + j);
@@ -54,7 +57,30 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojbutton', 'ojs/ojinputtext', '
                             result.series[i].items.push(colonnes[i]);
                     }
                 }
+                return result;
+            }
 
+            function stringToScatterChart(data) {
+                var result = {series: [], groups: []};
+                var lines = data.split("\\n");// split the data string into separated lines
+                var headers = lines[0].split("\\t"); // split the first line that contains series' names
+                for (var i = 0; i < headers.length; i++)
+                    result.series[i] = {name: headers[i].length === 0 ? "Serie " + (i + 1) : headers[i], items: []};
+                for (var i = 1; i < lines.length; i++)
+                    result.groups.push("Q" + (i));
+                for (var i = 0; i < result.series.length; i++) {
+                    for (var j = 1; j < lines.length; j++) {
+                        //console.log("L" + j);
+                        var colonnes = lines[j].split("\\t");
+                        //console.log("insert in items of S" + i + " : col" + i);
+                        //console.log("colonne = " + colonnes[i]);
+                        if (colonnes[i] !== undefined) {
+                            var coord = colonnes[i].split(",");
+                            //console.log("x = " + coord[0] + ",y = " + coord[1]);
+                            result.series[i].items.push({x: coord[0], y: coord[1]});
+                        }
+                    }
+                }
                 return result;
             }
 
