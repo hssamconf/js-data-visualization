1f847ef40c07aba7b4dadb55bb4099987acb11e1
Houssam-Eddine RAFIKI <hssamconf@gmail.com>
2018-06-27 11:39:42 +0100

    Finishing area chart

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -6,20 +6,25 @@
 /*
  * Your dashboard ViewModel code goes here
  */
-define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojbutton', 'ojs/ojinputtext', 'ojs/ojlabel'],
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojbutton', 'ojs/ojinputtext', 'ojs/ojlabel', 'ojs/ojchart'],
     function (oj, ko, $) {
 
         function DashboardViewModel() {
             var self = this;
             // Below are a set of the ViewModel methods invoked by the oj-module component.
             // Please reference the oj-module jsDoc for additional information.
-            this.data = ko.observable("ColumnA\\tColumnB\\tColumnC\\tColumnD\\n1\\t2\\t3\\t4\\na\\tb\\tc\\td");
+            this.dataAreaChart = ko.observable("Series 1\\tSeries 2\\tSeries 3\\tSeries 4\\n74\\t50\\t34\\t18\\n42\\t58\\t22\\t6\\n70\\t46\\t30\\t14\\n46\\t54\\t32\\t22");
+            this.data = ko.observable();
+            self.areaSeriesValue = ko.observableArray([]);
+            self.areaGroupsValue = ko.observableArray([]);
 
             self.submitInput = function () {
                 var element1 = document.getElementById("text-input");
                 element1.validate().then(function (result1) {
                     if (result1 === "valid") {
-                        console.log(stringToAreaChart(element1.value));
+                        var res = stringToAreaChart(element1.value);
+                        self.areaSeriesValue(res.series);
+                        self.areaGroupsValue(res.groups);
                     }
                 });
             };
@@ -35,11 +40,12 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojbutton', 'ojs/ojinputtext', '
 
                 for (var i = 0; i < headers.length; i++) {
                     result.series[i] = {name: headers[i].length === 0 ? "Serie " + (i + 1) : headers[i], items: []};
-                    result.groups.push("Q" + (i + 1));
                 }
 
+                for(var i = 1; i<lines.length; i++)
+                    result.groups.push("Q" + (i));
+
                 for (var i = 0; i < result.series.length; i++) {
-                    //console.log("S" + i);
                     for (var j = 1; j < lines.length; j++) {
                         //console.log("L" + j);
                         var colonnes = lines[j].split("\\t");
